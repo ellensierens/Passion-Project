09b21e5210f0e4752f5ebc8f1144e19063d7f46b
ellen sierens <ellen.sierens@student.howest.be>
2020-12-03 16:31:24 +0100

    opkuisen overbodige files (ml5 poging + tflite poging)

--- convert.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-converter = tf.lite.TFLiteConverter.from_saved_model("spice_1")
-converter.target_spec.supported_ops = [
-  tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
-  tf.lite.OpsSet.SELECT_TF_OPS # enable TensorFlow ops.
-]
-tflite_model = converter.convert()
-open("converted_model_1.tflite", "wb").write(tflite_model)
